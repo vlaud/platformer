f1e3484632752afa0593c2b75b615513694b36e8
vlaud <whehdgus1207@naver.com>
2024-04-14 18:51:23 +0900

    portal script

--- platformer/Assets/Scripts/Interactables/ButtonPedal.cs
+++ platformer/Assets/Scripts/Interactables/ButtonPedal.cs
@@ -8,16 +8,19 @@ public class ButtonPedal : MonoBehaviour
     [SerializeField] private Color _changeColor;
     [SerializeField] private LayerMask interactables;
     [SerializeField] private Transform _clickPedal;
+    [SerializeField] private Portal _portal;
 
     private void Update()
     {
         if(_clickPedal.localPosition.y < 0f)
         {
             _clickPedal.GetComponent<SpriteRenderer>().color = _changeColor;
+            _portal.SetGate(true);
         }
         else
         {
             _clickPedal.GetComponent<SpriteRenderer>().color = _originColor;
+            _portal.SetGate(false);
         }
     }
 }
---
+++ platformer/Assets/Scripts/Portal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] private Color _originColor;
+    ParticleSystem ps;
+
+    private void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+        SetGate(false);
+    }
+
+    public void SetGate(bool v)
+    {
+        var settings = ps.main;
+        settings.startColor = v ? _originColor : new Color(_originColor.r, _originColor.g, _originColor.b, 0f);
+    }
+}
